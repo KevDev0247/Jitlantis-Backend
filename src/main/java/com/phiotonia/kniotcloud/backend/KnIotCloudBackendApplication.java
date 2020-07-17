9cd4cc9cfb91f78a49cff3012d5ae0ab62216305
KevDev0247 <donkevin1980s@gmail.com>
2020-07-17 16:55:33 -0400

    Project creation

---
+++ src/main/java/com/phiotonia/kniotcloud/backend/KnIotCloudBackendApplication.java
@@ -0,0 +1,13 @@
+package com.phiotonia.kniotcloud.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnIotCloudBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnIotCloudBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phiotonia/kniotcloud/backend/KnIotCloudBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phiotonia.kniotcloud.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnIotCloudBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

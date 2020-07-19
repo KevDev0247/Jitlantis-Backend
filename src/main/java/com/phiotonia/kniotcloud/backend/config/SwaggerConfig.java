946f5ada5f0a8ab25210cb72f25cce692559f326
KevDev0247 <donkevin1980s@gmail.com>
2020-07-19 12:29:04 -0400

    Swagger configuration

--- src/main/java/com/phiotonia/kniotcloud/backend/config/SwaggerConfig.java
+++ src/main/java/com/phiotonia/kniotcloud/backend/config/SwaggerConfig.java
@@ -1,4 +1,44 @@
 package com.phiotonia.kniotcloud.backend.config;
 
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
+
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.phiotonia.kniotcloud.backend.controller"))
+                .build().globalOperationParameters(setHeaderToken());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API documentation")
+                .description("KNIotCloud Platform")
+                .termsOfServiceUrl("https://www.campusbell.cn")
+                .build();
+    }
+
+    private List<Parameter> setHeaderToken() {
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<>();
+        tokenPar.name("Authorization").description("token").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        pars.add(tokenPar.build());
+        return pars;
+    }
 }

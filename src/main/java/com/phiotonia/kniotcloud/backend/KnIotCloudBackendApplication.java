4770579479a518283c63896b04f1109492091c7d
KevDev0247 <donkevin1980s@gmail.com>
2020-07-20 16:16:59 -0400

    Main application

--- src/main/java/com/phiotonia/kniotcloud/backend/KnIotCloudBackendApplication.java
+++ src/main/java/com/phiotonia/kniotcloud/backend/KnIotCloudBackendApplication.java
@@ -4,11 +4,18 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableCaching
-@MapperScan("com.phiotonia.kniotcloud.backend")
+@MapperScan("com.phiotonia.kniotcloud.backend.dao")
 public class KnIotCloudBackendApplication {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(KnIotCloudBackendApplication.class, args);

c3d37efc2ecbae73e0869942969e41c192a78b36
KevDev0247 <donkevin1980s@gmail.com>
2020-07-21 14:22:30 -0400

    SysUser add uername field

--- src/main/java/com/phiotonia/kniotcloud/backend/model/SysUser.java
+++ src/main/java/com/phiotonia/kniotcloud/backend/model/SysUser.java
@@ -16,6 +16,7 @@ public class SysUser implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String name;
+    private String username;
     private String email;
     private String password;
     private Integer roleId;
@@ -33,6 +34,10 @@ public class SysUser implements Serializable {
 
     public String getName() {
         return name;
+    }
+
+    public String getUsername() {
+        return username;
     }
 
     public String getEmail() {
@@ -65,6 +70,10 @@ public class SysUser implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public void setEmail(String email) {

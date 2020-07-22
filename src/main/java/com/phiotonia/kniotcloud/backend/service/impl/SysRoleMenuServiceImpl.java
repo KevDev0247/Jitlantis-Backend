8394e58e5115a807651dbd8d03488a90a425d416
KevDev0247 <donkevin1980s@gmail.com>
2020-07-22 13:18:32 -0400

    Role section service implementation

--- src/main/java/com/phiotonia/kniotcloud/backend/service/impl/SysRoleMenuServiceImpl.java
+++ src/main/java/com/phiotonia/kniotcloud/backend/service/impl/SysRoleMenuServiceImpl.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import com.phiotonia.kniotcloud.backend.dao.SysRoleMenuDao;
 import com.phiotonia.kniotcloud.backend.model.SysRoleMenu;
 import com.phiotonia.kniotcloud.backend.service.SysRoleMenuService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SysRoleMenuServiceImpl extends ServiceImpl<SysRoleMenuDao, SysRoleMenu> implements SysRoleMenuService {
 }
--- src/main/java/com/phiotonia/kniotcloud/backend/service/impl/SysRoleServiceImpl.java
+++ src/main/java/com/phiotonia/kniotcloud/backend/service/impl/SysRoleServiceImpl.java
@@ -3,6 +3,9 @@ package com.phiotonia.kniotcloud.backend.service.impl;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import com.phiotonia.kniotcloud.backend.dao.SysRoleDao;
 import com.phiotonia.kniotcloud.backend.model.SysRole;
+import com.phiotonia.kniotcloud.backend.service.SysRoleService;
+import org.springframework.stereotype.Service;
 
-public class SysRoleServiceImpl extends ServiceImpl<SysRoleDao, SysRole> {
+@Service
+public class SysRoleServiceImpl extends ServiceImpl<SysRoleDao, SysRole> implements SysRoleService {
 }

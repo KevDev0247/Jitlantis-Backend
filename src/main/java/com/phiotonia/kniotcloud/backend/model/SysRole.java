d685e6ed2244c53ea2fdc610ea4bbdb16afc6968
KevDev0247 <donkevin1980s@gmail.com>
2020-07-22 15:18:02 -0400

    Add annotation

--- src/main/java/com/phiotonia/kniotcloud/backend/model/SysRole.java
+++ src/main/java/com/phiotonia/kniotcloud/backend/model/SysRole.java
@@ -2,10 +2,17 @@ package com.phiotonia.kniotcloud.backend.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
 
+@Entity
 public class SysRole {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private Integer type;
     private String roleName;

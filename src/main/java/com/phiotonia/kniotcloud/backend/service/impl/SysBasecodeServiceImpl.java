c27c9258631468720947501c9403b5661211a751
KevDev0247 <donkevin1980s@gmail.com>
2020-07-23 00:53:22 -0400

    SysBasecode implementation

---
+++ src/main/java/com/phiotonia/kniotcloud/backend/service/impl/SysBasecodeServiceImpl.java
@@ -0,0 +1,9 @@
+package com.phiotonia.kniotcloud.backend.service.impl;
+
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import com.phiotonia.kniotcloud.backend.dao.SysBasecodeDao;
+import com.phiotonia.kniotcloud.backend.model.SysBasecode;
+import com.phiotonia.kniotcloud.backend.service.SysBasecodeService;
+
+public class SysBasecodeServiceImpl extends ServiceImpl<SysBasecodeDao, SysBasecode> implements SysBasecodeService {
+}

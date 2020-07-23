83bfc1b0fd80cf5a9e21883303fa8c28f32dcaba
KevDev0247 <donkevin1980s@gmail.com>
2020-07-23 00:31:21 -0400

    Create client section Service interfaces

---
+++ src/main/java/com/phiotonia/kniotcloud/backend/service/ContactService.java
@@ -0,0 +1,7 @@
+package com.phiotonia.kniotcloud.backend.service;
+
+import com.baomidou.mybatisplus.service.IService;
+import com.phiotonia.kniotcloud.backend.model.Contact;
+
+public interface ContactService extends IService<Contact> {
+}

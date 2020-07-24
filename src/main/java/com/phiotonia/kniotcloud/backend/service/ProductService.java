2a9d505fc319867c65444872bb472ea1cb11f258
KevDev0247 <donkevin1980s@gmail.com>
2020-07-24 16:42:54 -0400

    ProductService

---
+++ src/main/java/com/phiotonia/kniotcloud/backend/service/ProductService.java
@@ -0,0 +1,7 @@
+package com.phiotonia.kniotcloud.backend.service;
+
+import com.baomidou.mybatisplus.service.IService;
+import com.phiotonia.kniotcloud.backend.model.Product;
+
+public interface ProductService extends IService<Product> {
+}

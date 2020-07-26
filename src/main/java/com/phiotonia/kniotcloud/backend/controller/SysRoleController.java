0f22f31b53e41a4be811302ab6c749e078ba783f
KevDev0247 <donkevin1980s@gmail.com>
2020-07-26 17:56:03 -0400

    SysRoleController javadoc comments

--- src/main/java/com/phiotonia/kniotcloud/backend/controller/SysRoleController.java
+++ src/main/java/com/phiotonia/kniotcloud/backend/controller/SysRoleController.java
@@ -23,6 +23,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * The controller for SysRole (System Role) that handles HTTP requests and responses.
+ * In this frontend-backend-separated architecture,
+ * the controller interacts with the particular service on the frontend.
+ *
+ * @author Kevin Zhijun Wang
+ * @version 2020.0715
+ */
 @Api(tags = {"role"})
 @RestController
 @RequestMapping("/role")

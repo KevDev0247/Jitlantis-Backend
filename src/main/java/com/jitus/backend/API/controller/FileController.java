ee8816bf240b1bfefe3d3f2d1c413b0c26c2912e
KevDev0247 <donkevin1980s@gmail.com>
2020-07-29 14:21:07 -0400

    Change upload logic

--- src/main/java/com/jitus/backend/API/controller/FileController.java
+++ src/main/java/com/jitus/backend/API/controller/FileController.java
@@ -1,8 +1,10 @@
 package com.jitus.backend.API.controller;
 
 import com.jitus.backend.API.model.SysAttachments;
+import com.jitus.backend.API.service.SysAttachmentsService;
 import com.jitus.backend.API.utils.OSSFileUtils;
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,22 +13,44 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.*;
 import java.util.Map;
+
+import static com.jitus.backend.API.utils.SystemConstants.UPLOAD_FILE_PATH;
 
 @Api(tags = {"file"})
 @RestController
 @RequestMapping("/file")
 public class FileController {
 
-    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
-    public ResponseEntity<Map<String, String>> uploadFile(@RequestParam("file") MultipartFile file) {
-        Map<String, String> map = OSSFileUtils.saveFile(file);
-        if (map != null && map.containsKey("r_code") && map.get("r_code").equals("1")) {
-            SysAttachments attachmentEntity = new SysAttachments();
-            attachmentEntity.setFileurl(map.get("file_url"));
-            attachmentEntity.setFilename(map.get("file_name"));
-        }
+    @Autowired
+    private SysAttachmentsService sysAttachmentsService;
 
-        return new ResponseEntity<>(map, HttpStatus.OK);
+    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+    public String uploadFile(@RequestParam("file") MultipartFile file) {
+        if (!file.isEmpty()) {
+            try {
+                String fileName = file.getOriginalFilename();
+                String extension = fileName.substring(fileName.lastIndexOf("."));
+                String fileUrl = UPLOAD_FILE_PATH + fileName;
+                BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(new File(fileUrl)));
+
+                outputStream.write(file.getBytes());
+                outputStream.flush();
+                outputStream.close();
+
+                SysAttachments sysAttachmentsEntity = new SysAttachments();
+                sysAttachmentsEntity.setFileurl(fileUrl);
+                sysAttachmentsEntity.setFilename(fileName);
+                sysAttachmentsEntity.setExtension(extension);
+                sysAttachmentsService.insert(sysAttachmentsEntity);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "upload failed" + e.getMessage();
+            }
+            return "upload successful";
+        } else {
+            return "upload failed, the file is empty";
+        }
     }
 }

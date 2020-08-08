9cb9a24b906229b3d606a241b916ae119fc6ff89
KevDev0247 <donkevin1980s@gmail.com>
2020-08-08 16:52:33 -0400

    Add selectQueryList method in SysRoleService

--- src/main/java/com/jitlantis/backend/API/service/SysRoleService.java
+++ src/main/java/com/jitlantis/backend/API/service/SysRoleService.java
@@ -3,5 +3,9 @@ package com.jitlantis.backend.API.service;
 import com.baomidou.mybatisplus.service.IService;
 import com.jitlantis.backend.API.model.SysRole;
 
+import java.util.List;
+
 public interface SysRoleService extends IService<SysRole> {
+
+    List<SysRole> selectQueryList(String roleName, String remark);
 }

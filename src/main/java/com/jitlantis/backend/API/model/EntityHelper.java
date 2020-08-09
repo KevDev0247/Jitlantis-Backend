2758fe75d896a1dc4713507c9d1f905d04736dfe
KevDev0247 <donkevin1980s@gmail.com>
2020-08-09 14:00:52 -0400

    Entity helper javadoc comments

--- src/main/java/com/jitlantis/backend/API/model/EntityHelper.java
+++ src/main/java/com/jitlantis/backend/API/model/EntityHelper.java
@@ -1,5 +1,11 @@
 package com.jitlantis.backend.API.model;
 
+/**
+ * The entity helper
+ *
+ * @author Kevin Zhijun Wang
+ * created on 2020/07/23
+ */
 public abstract class EntityHelper {
 
     public abstract String getPrimaryKey();

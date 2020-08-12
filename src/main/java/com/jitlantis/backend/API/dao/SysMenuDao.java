e583300cbd51f6e28a225494d1ae45adf8564221
KevDev0247 <donkevin1980s@gmail.com>
2020-08-12 17:52:09 -0400

    Add methods in SysMenuDao

--- src/main/java/com/jitlantis/backend/API/dao/SysMenuDao.java
+++ src/main/java/com/jitlantis/backend/API/dao/SysMenuDao.java
@@ -1,9 +1,19 @@
 package com.jitlantis.backend.API.dao;
 
 import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.jitlantis.backend.API.dto.BaseMenuDto;
 import com.jitlantis.backend.API.model.SysMenu;
 import org.mapstruct.Mapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Mapper
 public interface SysMenuDao extends BaseMapper<SysMenu> {
+
+    List<SysMenu> selectMenuListByRoleId(@Param("roleId") Integer roleId);
+
+    List<BaseMenuDto> selectFirstMenuDtoList();
 }

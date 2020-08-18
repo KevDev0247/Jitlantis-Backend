aac577cf416f0897a7ebcf5d6ccd31d64ca2dde3
KevDev0247 <donkevin1980s@gmail.com>
2020-08-18 14:54:44 -0400

    Fix organization 500 error

--- src/main/java/com/jitlantis/backend/API/dao/SysOrganizationDao.java
+++ src/main/java/com/jitlantis/backend/API/dao/SysOrganizationDao.java
@@ -3,8 +3,8 @@ package com.jitlantis.backend.API.dao;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.jitlantis.backend.API.dto.OrgDto;
 import com.jitlantis.backend.API.model.SysOrganization;
+import org.apache.ibatis.annotations.Param;
 import org.mapstruct.Mapper;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 
 import java.util.List;

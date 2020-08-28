396889d585df58d71548352485bf6ffa954050dd
KevDev0247 <donkevin1980s@gmail.com>
2020-08-27 21:43:06 -0400

    Add methods to get list from entity

--- src/main/java/com/jitlantis/backend/API/base/WolfConverter.java
+++ src/main/java/com/jitlantis/backend/API/base/WolfConverter.java
@@ -1,7 +1,10 @@
 package com.jitlantis.backend.API.base;
 
+import cn.hutool.core.date.DatePattern;
+import cn.hutool.core.date.format.FastDateFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,6 +26,10 @@ public class WolfConverter {
         return null;
     }
 
+    public <T> T convertObjectToBean(Object bean, Class<T> clazz) {
+        return WolfBeanUtil.mapToBeanIgnoreCase(convertBeanToMap(bean), clazz, true);
+    }
+
     public Map<String, Object> convertBeanToMap(Object bean) {
         if (bean != null) {
             return WolfBeanUtil.beanToMap(bean, false, false);
@@ -42,6 +49,111 @@ public class WolfConverter {
         if (t != null && bean != null) {
             WolfBeanUtil.copyProperties(bean, t);
             return t;
+        }
+        return null;
+    }
+
+    public List<Long> getLongListFromEntityList(List<?> entityList, String field) {
+        if (entityList != null && entityList.size() > 0) {
+            List<Long> list = new ArrayList<>();
+            String tmpField = "id";
+            if (field != null && field.trim().length() > 0) {
+                tmpField = field.trim();
+            }
+            for (Object object : entityList) {
+                Map<String, Object> map = convertBeanToMap(object);
+                if (map.containsKey(tmpField) && !StringUtils.isEmpty(map.get(tmpField))) {
+                    Long fieldValue = Long.parseLong(String.valueOf(map.get(tmpField)));
+                    if (!list.contains(fieldValue)) {
+                        list.add(fieldValue);
+                    }
+                }
+            }
+            return list;
+        }
+        return null;
+    }
+
+    public List<String> getStringListFromEntityList(List<?> entityList, String field) {
+        if (entityList != null && entityList.size() > 0) {
+            List<String> list = new ArrayList<>();
+            String tmpField = "id";
+            if (field != null && field.trim().length() > 0) {
+                tmpField = field.trim();
+            }
+            for (Object object : entityList) {
+                Map<String, Object> map = convertBeanToMap(object);
+                if (map.containsKey(tmpField)) {
+                    String fieldValue = String.valueOf(map.get(tmpField));
+                    if (!list.contains(fieldValue) && fieldValue.length() > 0) {
+                        list.add(fieldValue);
+                    }
+                }
+            }
+            return list;
+        }
+        return null;
+    }
+
+    public List<Long> getCommaStringListFromEntityList(List<?> entityList, String field) {
+        if (entityList != null && entityList.size() > 0) {
+            List<Long> list = new ArrayList<>();
+            String tmpField = "mixspecids";
+            if (field != null && field.trim().length() > 0) {
+                tmpField = field.trim();
+            }
+            for (Object object : entityList) {
+                Map<String, Object> map = convertBeanToMap(object);
+                if (map.containsKey(tmpField)) {
+                    String fieldValue = String.valueOf(map.get(tmpField));
+                    String[] result = fieldValue.split(",");
+                    for (String string : result) {
+                        Long element = Long.valueOf(string);
+                        if (!list.contains(element)) {
+                            list.add(element);
+                        }
+                    }
+                }
+            }
+            return list;
+        }
+        return null;
+    }
+
+    public <T> List<T> getDateListFromEntityList(Class<T> clazz, List<?> entityList, String dateField, String formatPattern) {
+        if (entityList != null && entityList.size() > 0) {
+            List<T> list = new ArrayList<>();
+            String tmpField = "createtime";
+            if (dateField != null && dateField.trim().length() > 0) {
+                tmpField = dateField.trim();
+            }
+            String pattern = DatePattern.NORM_DATE_PATTERN;
+            if (formatPattern != null && formatPattern.trim().length() > 0) {
+                pattern = formatPattern.trim();
+            }
+            for (Object object : entityList) {
+                T t = convertObjectToBean(object, clazz);
+                Map<String, Object> map = convertBeanToMap(object);
+                if (map.containsKey(tmpField)) {
+                    String fieldValue = FastDateFormat.getInstance(pattern).format(map.get(tmpField));
+                    WolfBeanUtil.setFieldValue(t, tmpField, fieldValue);
+                }
+                list.add(t);
+            }
+            return list;
+        }
+        return null;
+    }
+
+    public List<Long> getLongListFromSingleEntity(String field) {
+        if (field != null && field.length() > 0) {
+            String[] result = field.split(",");
+            List<Long> list = new ArrayList<>();
+            for (String string : result) {
+                Long element = Long.valueOf(string);
+                list.add(element);
+            }
+            return list;
         }
         return null;
     }

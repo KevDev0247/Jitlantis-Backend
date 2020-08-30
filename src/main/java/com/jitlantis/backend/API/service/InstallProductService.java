1d1b8e922c95a4eae3b98ee69b5049fa6af57ed7
KevDev0247 <donkevin1980s@gmail.com>
2020-08-29 22:14:59 -0400

    Create InstallProductService

---
+++ src/main/java/com/jitlantis/backend/API/service/InstallProductService.java
@@ -0,0 +1,7 @@
+package com.jitlantis.backend.API.service;
+
+import com.baomidou.mybatisplus.service.IService;
+import com.jitlantis.backend.API.model.Evaluate;
+
+public interface InstallProductService extends IService<Evaluate> {
+}

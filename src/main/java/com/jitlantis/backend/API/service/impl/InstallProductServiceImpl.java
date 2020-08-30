71f4c5629bacde7b877ba62b565a81777a860cd2
KevDev0247 <donkevin1980s@gmail.com>
2020-08-29 22:43:27 -0400

    Create InstallProduct and Evaluate Service implementations

---
+++ src/main/java/com/jitlantis/backend/API/service/impl/InstallProductServiceImpl.java
@@ -0,0 +1,9 @@
+package com.jitlantis.backend.API.service.impl;
+
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import com.jitlantis.backend.API.dao.InstallProductDao;
+import com.jitlantis.backend.API.model.InstallProduct;
+import com.jitlantis.backend.API.service.InstallProductService;
+
+public class InstallProductServiceImpl extends ServiceImpl<InstallProductDao, InstallProduct> implements InstallProductService {
+}

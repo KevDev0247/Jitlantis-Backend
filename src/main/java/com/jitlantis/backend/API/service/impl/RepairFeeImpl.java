9fcfac06607a104fbb5d3b928d8e4ec1aca93986
KevDev0247 <donkevin1980s@gmail.com>
2020-08-29 22:44:20 -0400

    Create repair service implementations

---
+++ src/main/java/com/jitlantis/backend/API/service/impl/RepairFeeImpl.java
@@ -0,0 +1,9 @@
+package com.jitlantis.backend.API.service.impl;
+
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import com.jitlantis.backend.API.dao.RepairFeeDao;
+import com.jitlantis.backend.API.model.RepairFee;
+import com.jitlantis.backend.API.service.RepairFeeService;
+
+public class RepairFeeImpl extends ServiceImpl<RepairFeeDao, RepairFee> implements RepairFeeService {
+}
---
+++ src/main/java/com/jitlantis/backend/API/service/impl/RepairServiceImpl.java
@@ -0,0 +1,9 @@
+package com.jitlantis.backend.API.service.impl;
+
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import com.jitlantis.backend.API.dao.RepairDao;
+import com.jitlantis.backend.API.model.Repair;
+import com.jitlantis.backend.API.service.RepairService;
+
+public class RepairServiceImpl extends ServiceImpl<RepairDao, Repair> implements RepairService {
+}

6384406bd72eb732aab413cbad64f20284bd84d7
KevDev0247 <donkevin1980s@gmail.com>
2020-08-29 22:43:53 -0400

    Fix wrong imports

--- src/main/java/com/jitlantis/backend/API/service/InstallProductService.java
+++ src/main/java/com/jitlantis/backend/API/service/InstallProductService.java
@@ -2,6 +2,7 @@ package com.jitlantis.backend.API.service;
 
 import com.baomidou.mybatisplus.service.IService;
 import com.jitlantis.backend.API.model.Evaluate;
+import com.jitlantis.backend.API.model.InstallProduct;
 
-public interface InstallProductService extends IService<Evaluate> {
+public interface InstallProductService extends IService<InstallProduct> {
 }

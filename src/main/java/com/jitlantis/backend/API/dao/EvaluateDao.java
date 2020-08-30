ce81ceca9a6c12efa16404d50d0e6f08514e36b4
KevDev0247 <donkevin1980s@gmail.com>
2020-08-29 22:29:08 -0400

    Create Dao for Evaluate and InstallProduct

---
+++ src/main/java/com/jitlantis/backend/API/dao/EvaluateDao.java
@@ -0,0 +1,9 @@
+package com.jitlantis.backend.API.dao;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.jitlantis.backend.API.model.Evaluate;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface EvaluateDao extends BaseMapper<Evaluate> {
+}
---
+++ src/main/java/com/jitlantis/backend/API/dao/InstallProductDao.java
@@ -0,0 +1,8 @@
+package com.jitlantis.backend.API.dao;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface InstallProductDao extends BaseMapper<InstallProductDao> {
+}

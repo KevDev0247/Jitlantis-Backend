3604e6e1621698d96361ae5b29246f06492110ae
KevDev0247 <donkevin1980s@gmail.com>
2020-09-02 17:01:49 -0400

    Create Service Implementations

---
+++ src/main/java/com/jitlantis/backend/API/service/impl/SendAttachmentServiceImpl.java
@@ -0,0 +1,9 @@
+package com.jitlantis.backend.API.service.impl;
+
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import com.jitlantis.backend.API.dao.SendAttachmentDao;
+import com.jitlantis.backend.API.model.SendAttachment;
+import com.jitlantis.backend.API.service.SendAttachmentService;
+
+public class SendAttachmentServiceImpl extends ServiceImpl<SendAttachmentDao, SendAttachment> implements SendAttachmentService {
+}
---
+++ src/main/java/com/jitlantis/backend/API/service/impl/SendProductServiceImpl.java
@@ -0,0 +1,9 @@
+package com.jitlantis.backend.API.service.impl;
+
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import com.jitlantis.backend.API.dao.SendProductDao;
+import com.jitlantis.backend.API.model.SendProduct;
+import com.jitlantis.backend.API.service.SendProductService;
+
+public class SendProductServiceImpl extends ServiceImpl<SendProductDao, SendProduct> implements SendProductService {
+}

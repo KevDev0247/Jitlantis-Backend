5ee6593fb60b265e79439212c010aef986d33123
KevDev0247 <donkevin1980s@gmail.com>
2020-09-02 16:55:35 -0400

    Create Service Interfaces

---
+++ src/main/java/com/jitlantis/backend/API/service/SendProductService.java
@@ -0,0 +1,7 @@
+package com.jitlantis.backend.API.service;
+
+import com.baomidou.mybatisplus.service.IService;
+import com.jitlantis.backend.API.model.SendProduct;
+
+public interface SendProductService extends IService<SendProduct> {
+}

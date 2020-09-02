bb57ebe5decb3daf21d6784f93ec0846c3b13acc
YonggangSu <ygsu2001@gmail.com>
2020-09-02 10:33:32 -0400

    Add method in SysUserService

--- src/main/java/com/jitlantis/backend/API/service/SysUserService.java
+++ src/main/java/com/jitlantis/backend/API/service/SysUserService.java
@@ -5,11 +5,19 @@ import com.jitlantis.backend.API.model.SysUser;
 
 import java.util.List;
 
+/**
+ * The Service interface for System Users.
+ *
+ * @author Kevin Zhijun Wang, Yonggang Su
+ * created on 2020/07/21
+ */
 public interface SysUserService extends IService<SysUser> {
 
     SysUser findUserByName(String name);
 
     List<SysUser> selectQueryList(String name, String email);
+
+    List<SysUser> selectClientQueryList(String name, String email);
 
     boolean delete(Integer userId);
 }

42e7c839518ae370459550786e2854beac2e6471
YonggangSu <ygsu2001@gmail.com>
2020-09-11 01:04:48 -0400

    Add i methods to query contract's optionList

--- src/main/java/com/jitlantis/backend/API/controller/ContractController.java
+++ src/main/java/com/jitlantis/backend/API/controller/ContractController.java
@@ -1,6 +1,7 @@
 package com.jitlantis.backend.API.controller;
 
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.jitlantis.backend.API.dto.BaseItemDto;
 import com.jitlantis.backend.API.model.Contract;
 import com.jitlantis.backend.API.service.ContractService;
 import com.jitlantis.backend.API.utils.DeletedEnum;
@@ -9,12 +10,14 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -134,6 +137,19 @@ public class ContractController {
         wrapper.eq("is_delete", DeletedEnum.N.value());
         wrapper.orderBy("id");
         map.put("list", contractService.selectList(wrapper));
+
+        return new ResponseEntity<>(map, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "query contract option list", notes = "No Pagination")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name = "name", value = "Contract Name")
+    })
+    @RequestMapping(value = "/optionList", method = RequestMethod.GET)
+    public ResponseEntity<Map<String, Object>> queryContractOptionList(@RequestParam(value = "name", required = false) String name) {
+        Map<String, Object> map = new HashMap<>();
+        List<BaseItemDto> baseItemList = contractService.optionList(name);
+        map.put("list", baseItemList);
 
         return new ResponseEntity<>(map, HttpStatus.OK);
     }

36233d8d78992b8cf9a3f7e93d0fad73cee595ac
KevDev0247 <donkevin1980s@gmail.com>
2020-09-10 23:52:25 -0400

    Create BaseItemDto

---
+++ src/main/java/com/jitlantis/backend/API/dto/BaseItemDto.java
@@ -0,0 +1,24 @@
+package com.jitlantis.backend.API.dto;
+
+public class BaseItemDto {
+
+    private Integer id;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

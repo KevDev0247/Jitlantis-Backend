958381cb3c0687fcb7a21882fc691f289bef40b6
YonggangSu <ygsu2001@gmail.com>
2020-09-14 15:45:43 -0400

    Fix controller API not updating issue and record testing logs

--- src/main/java/com/jitlantis/backend/API/controller/RepairRecordController.java
+++ src/main/java/com/jitlantis/backend/API/controller/RepairRecordController.java
@@ -57,7 +57,7 @@ public class RepairRecordController {
         if (repairrecordSelected == null) {
             response = false;
         } else {
-            response = repairrecordService.updateById(repairrecordSelected);
+            response = repairrecordService.updateById(repairrecord);
         }
         map.put("data", response);
 

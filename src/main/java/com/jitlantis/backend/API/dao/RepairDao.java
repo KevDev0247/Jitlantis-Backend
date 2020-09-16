fa8eef0ed003280d3c01c811386e3a2aa03ec3c2
KevDev0247 <donkevin1980s@gmail.com>
2020-09-16 15:58:18 -0400

    Create selectPageList method in DAO

--- src/main/java/com/jitlantis/backend/API/dao/RepairDao.java
+++ src/main/java/com/jitlantis/backend/API/dao/RepairDao.java
@@ -1,9 +1,17 @@
 package com.jitlantis.backend.API.dao;
 
 import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.baomidou.mybatisplus.plugins.Page;
 import com.jitlantis.backend.API.model.Repair;
+import org.apache.ibatis.annotations.Param;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Mapper
+@Component
 public interface RepairDao extends BaseMapper<Repair> {
+
+    List<Repair> selectPageList(Page<Repair> page, @Param("status") Integer status, @Param("search") String search);
 }

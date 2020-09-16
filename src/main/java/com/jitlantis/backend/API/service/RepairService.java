dce6a564d023f3f8dbf7b5573175bae4c0668f41
KevDev0247 <donkevin1980s@gmail.com>
2020-09-16 15:57:58 -0400

    Create selectPageList method in service interface and implementation

--- src/main/java/com/jitlantis/backend/API/service/RepairService.java
+++ src/main/java/com/jitlantis/backend/API/service/RepairService.java
@@ -1,7 +1,10 @@
 package com.jitlantis.backend.API.service;
 
+import com.baomidou.mybatisplus.plugins.Page;
 import com.baomidou.mybatisplus.service.IService;
 import com.jitlantis.backend.API.model.Repair;
 
 public interface RepairService extends IService<Repair> {
+
+    Page<Repair> selectPageList(int pageNum, int pageSize, Integer status, String search);
 }

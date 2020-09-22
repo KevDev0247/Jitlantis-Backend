533a654a64bc611c66bc2e9944c6f5002c874ba4
YonggangSu <ygsu2001@gmail.com>
2020-09-22 14:53:15 -0400

    Add in optionList method in service interface

--- src/main/java/com/jitlantis/backend/API/service/ProductService.java
+++ src/main/java/com/jitlantis/backend/API/service/ProductService.java
@@ -1,7 +1,12 @@
 package com.jitlantis.backend.API.service;
 
 import com.baomidou.mybatisplus.service.IService;
+import com.jitlantis.backend.API.dto.BaseItemDto;
 import com.jitlantis.backend.API.model.Product;
 
+import java.util.List;
+
 public interface ProductService extends IService<Product> {
+
+    List<BaseItemDto> optionList(String name);
 }

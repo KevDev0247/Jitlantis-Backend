abb09b1fc5d66be7fc4d23976bf55e62dccd0195
YonggangSu <ygsu2001@gmail.com>
2020-09-23 00:33:16 -0400

    Testing logs and add method

--- src/main/java/com/jitlantis/backend/API/dao/ProjectDao.java
+++ src/main/java/com/jitlantis/backend/API/dao/ProjectDao.java
@@ -3,7 +3,11 @@ package com.jitlantis.backend.API.dao;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.jitlantis.backend.API.model.Project;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
 @Mapper
+@Component
 public interface ProjectDao extends BaseMapper<Project> {
+
+    Project selectById(Integer id);
 }

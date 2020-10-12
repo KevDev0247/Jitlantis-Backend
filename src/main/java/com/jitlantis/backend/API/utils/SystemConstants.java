2b5c32b53a37a13d10dcae18762bbf2742080996
YonggangSu <ygsu2001@gmail.com>
2020-10-12 18:37:48 -0400

    Remove unnecessary variables

--- src/main/java/com/jitlantis/backend/API/utils/SystemConstants.java
+++ src/main/java/com/jitlantis/backend/API/utils/SystemConstants.java
@@ -1,7 +1,7 @@
 package com.jitlantis.backend.API.utils;
 
 public class SystemConstants {
-    public static final String UPLOAD_FILE_PATH = "http://52.229.167.195:8080//FilesUpload//";
+    public static final String UPLOAD_FILE_PATH = "";
     public static final Integer BUSSINESSCLIENT = 7;
     public static final Integer HOMECLIENT = 8;
     public static final Integer PROJRCTCLIENT = 9;

70ce05175c79ea1331b6bf9e530407d8350505cc
KevDev0247 <donkevin1980s@gmail.com>
2020-10-12 23:49:28 -0400

    Create SysMessageService implementation

---
+++ src/main/java/com/jitlantis/backend/API/service/impl/SysMessageServiceImpl.java
@@ -0,0 +1,9 @@
+package com.jitlantis.backend.API.service.impl;
+
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import com.jitlantis.backend.API.dao.SysMessageDao;
+import com.jitlantis.backend.API.model.SysMessage;
+import com.jitlantis.backend.API.service.SysMessageService;
+
+public class SysMessageServiceImpl extends ServiceImpl<SysMessageDao, SysMessage> implements SysMessageService {
+}

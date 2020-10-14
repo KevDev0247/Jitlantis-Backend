f853497bd5f53c955f0c916702031ae76357b317
KevDev0247 <donkevin1980s@gmail.com>
2020-10-13 20:12:12 -0400

    Fix SysMessageService dependency injection error

--- src/main/java/com/jitlantis/backend/API/service/impl/SysMessageServiceImpl.java
+++ src/main/java/com/jitlantis/backend/API/service/impl/SysMessageServiceImpl.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import com.jitlantis.backend.API.dao.SysMessageDao;
 import com.jitlantis.backend.API.model.SysMessage;
 import com.jitlantis.backend.API.service.SysMessageService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SysMessageServiceImpl extends ServiceImpl<SysMessageDao, SysMessage> implements SysMessageService {
 }

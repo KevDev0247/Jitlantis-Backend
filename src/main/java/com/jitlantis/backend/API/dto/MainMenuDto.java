6e97d52569dc57dd938779f248504436adaba5a1
KevDev0247 <donkevin1980s@gmail.com>
2020-10-31 14:30:41 -0400

    Create Dto MainMenu

---
+++ src/main/java/com/jitlantis/backend/API/dto/MainMenuDto.java
@@ -0,0 +1,14 @@
+package com.jitlantis.backend.API.dto;
+
+public class MainMenuDto {
+
+    private Integer isShow;
+
+    public Integer getIsShow() {
+        return isShow;
+    }
+
+    public void setIsShow(Integer isShow) {
+        this.isShow = isShow;
+    }
+}

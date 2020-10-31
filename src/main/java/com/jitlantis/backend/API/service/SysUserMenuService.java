e1b994fd16a677967aaceb2bce872ac1529d8939
KevDev0247 <donkevin1980s@gmail.com>
2020-10-31 15:32:44 -0400

    Create interface for Service class of SysUserMenu

---
+++ src/main/java/com/jitlantis/backend/API/service/SysUserMenuService.java
@@ -0,0 +1,12 @@
+package com.jitlantis.backend.API.service;
+
+import com.baomidou.mybatisplus.service.IService;
+import com.jitlantis.backend.API.dto.MainMenuDto;
+import com.jitlantis.backend.API.model.SysUserMenu;
+
+import java.util.List;
+
+public interface SysUserMenuService extends IService<SysUserMenu> {
+
+    List<MainMenuDto> getMainMenus(Integer userId);
+}

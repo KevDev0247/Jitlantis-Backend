53a78c51f26ddc0c10aca4ad5c36c55579327d49
KevDev0247 <donkevin1980s@gmail.com>
2020-10-31 23:47:18 -0400

    Update MainMenuDto by adding umid field

--- src/main/java/com/jitlantis/backend/API/dto/MainMenuDto.java
+++ src/main/java/com/jitlantis/backend/API/dto/MainMenuDto.java
@@ -4,11 +4,21 @@ public class MainMenuDto {
 
     private Integer isShow;
 
+    private Integer umid;
+
     public Integer getIsShow() {
         return isShow;
     }
 
+    public Integer getUmid() {
+        return umid;
+    }
+
     public void setIsShow(Integer isShow) {
         this.isShow = isShow;
+    }
+
+    public void setUmid(Integer umid) {
+        this.umid = umid;
     }
 }

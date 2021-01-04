99897f6e26fc378c54710c524bd3d30ebbc27027
KevDev0247 <donkevin1980s@gmail.com>
2021-01-04 11:55:46 -0500

    Create Basecode service interface

--- src/main/java/com/jitlantis/backend/API/service/SysBasecodeService.java
+++ src/main/java/com/jitlantis/backend/API/service/SysBasecodeService.java
@@ -3,5 +3,8 @@ package com.jitlantis.backend.API.service;
 import com.baomidou.mybatisplus.service.IService;
 import com.jitlantis.backend.API.model.SysBasecode;
 
+import java.util.List;
+
 public interface SysBasecodeService extends IService<SysBasecode> {
+    List<SysBasecode> optionList(String typeId, String info);
 }

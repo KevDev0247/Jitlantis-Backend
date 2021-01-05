d5c94fd1c3a63e4c9abc6c920e2a65eb648bf623
KevDev0247 <donkevin1980s@gmail.com>
2021-01-04 23:34:39 -0500

    CtaFollow javadoc

--- src/main/java/com/jitlantis/backend/API/service/impl/CtaFollowServiceImpl.java
+++ src/main/java/com/jitlantis/backend/API/service/impl/CtaFollowServiceImpl.java
@@ -6,6 +6,12 @@ import com.jitlantis.backend.API.model.CtaFollow;
 import com.jitlantis.backend.API.service.CtaFollowService;
 import org.springframework.stereotype.Service;
 
+/**
+ * The implementation of the Service interface for CtaFollow
+ *
+ * @author Kevin Zhijun Wang
+ * created on 2020/07/24
+ */
 @Service
 public class CtaFollowServiceImpl extends ServiceImpl<CtaFollowDao, CtaFollow> implements CtaFollowService {
 }

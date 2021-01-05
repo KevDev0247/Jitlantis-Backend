e1371c5259a77a3c85521de458caf509082c0a7e
KevDev0247 <donkevin1980s@gmail.com>
2021-01-04 21:14:50 -0500

    Create DAO for SysUserRole

---
+++ src/main/java/com/jitlantis/backend/API/dao/SysUserRoleDao.java
@@ -0,0 +1,9 @@
+package com.jitlantis.backend.API.dao;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.jitlantis.backend.API.model.SysUserRole;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SysUserRoleDao extends BaseMapper<SysUserRole> {
+}

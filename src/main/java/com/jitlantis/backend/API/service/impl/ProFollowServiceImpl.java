dc1938789c44f070a1839eb01d70be4000b2150b
KevDev0247 <donkevin1980s@gmail.com>
2021-01-04 23:42:18 -0500

    Profollow Service update

--- src/main/java/com/jitlantis/backend/API/service/impl/ProFollowServiceImpl.java
+++ src/main/java/com/jitlantis/backend/API/service/impl/ProFollowServiceImpl.java
@@ -6,6 +6,12 @@ import com.jitlantis.backend.API.model.ProFollow;
 import com.jitlantis.backend.API.service.ProFollowService;
 import org.springframework.stereotype.Service;
 
+/**
+ * The implementation of the Service interface for ProFollow
+ *
+ * @author Kevin Zhijun Wang
+ * created on 2020/07/27
+ */
 @Service
 public class ProFollowServiceImpl extends ServiceImpl<ProFollowDao, ProFollow> implements ProFollowService {
 }

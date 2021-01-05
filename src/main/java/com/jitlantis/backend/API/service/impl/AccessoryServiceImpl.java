0895c0e90ecaa282b69d99e1e4fea27502c9619a
KevDev0247 <donkevin1980s@gmail.com>
2021-01-04 23:33:52 -0500

    Accessory javadoc

--- src/main/java/com/jitlantis/backend/API/service/impl/AccessoryServiceImpl.java
+++ src/main/java/com/jitlantis/backend/API/service/impl/AccessoryServiceImpl.java
@@ -6,6 +6,12 @@ import com.jitlantis.backend.API.model.Accessory;
 import com.jitlantis.backend.API.service.AccessoryService;
 import org.springframework.stereotype.Service;
 
+/**
+ * The implementation of the Service interface for Accessory
+ *
+ * @author Kevin Zhijun Wang
+ * created on 2020/09/10
+ */
 @Service
 public class AccessoryServiceImpl extends ServiceImpl<AccessoryDao, Accessory> implements AccessoryService {
 }

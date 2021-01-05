6101e8995c874ebc343e57b5b2cb7a0c6d3c24dd
KevDev0247 <donkevin1980s@gmail.com>
2021-01-04 21:09:35 -0500

    Javadoc comments for MyLog

--- src/main/java/com/jitlantis/backend/API/annotation/MyLog.java
+++ src/main/java/com/jitlantis/backend/API/annotation/MyLog.java
@@ -1,9 +1,16 @@
 package com.jitlantis.backend.API.annotation;
 
 import com.jitlantis.backend.API.enums.BusinessType;
+import com.jitlantis.backend.API.model.Accessory;
 
 import java.lang.annotation.*;
 
+/**
+ * A customized annotation for logs
+ *
+ * @author Kevin Zhijun Wang
+ * created on 2020/08/19
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented

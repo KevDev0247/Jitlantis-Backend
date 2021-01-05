af788470eb1fa3502b0ce3a96749a2899ee2d1e1
KevDev0247 <donkevin1980s@gmail.com>
2021-01-04 23:34:03 -0500

    ClientProduct javadoc

--- src/main/java/com/jitlantis/backend/API/service/impl/ClientProductServiceImpl.java
+++ src/main/java/com/jitlantis/backend/API/service/impl/ClientProductServiceImpl.java
@@ -6,6 +6,12 @@ import com.jitlantis.backend.API.model.ClientProduct;
 import com.jitlantis.backend.API.service.ClientProductService;
 import org.springframework.stereotype.Service;
 
+/**
+ * The implementation of the Service interface for ClientProduct
+ *
+ * @author Kevin Zhijun Wang
+ * created on 2020/07/24
+ */
 @Service
 public class ClientProductServiceImpl extends ServiceImpl<ClientProductDao, ClientProduct> implements ClientProductService {
 }

1b6caa455f96f6388e26aea90f4908809fa076e8
KevDev0247 <donkevin1980s@gmail.com>
2021-01-04 23:41:51 -0500

    Prodocument javadoc

--- src/main/java/com/jitlantis/backend/API/service/impl/ProDocumentServiceImpl.java
+++ src/main/java/com/jitlantis/backend/API/service/impl/ProDocumentServiceImpl.java
@@ -6,6 +6,12 @@ import com.jitlantis.backend.API.model.ProDocument;
 import com.jitlantis.backend.API.service.ProDocumentService;
 import org.springframework.stereotype.Service;
 
+/**
+ * The implementation of the Service interface for ProDocument
+ *
+ * @author Kevin Zhijun Wang
+ * created on 2020/07/27
+ */
 @Service
 public class ProDocumentServiceImpl extends ServiceImpl<ProDocumentDao, ProDocument> implements ProDocumentService {
 }
